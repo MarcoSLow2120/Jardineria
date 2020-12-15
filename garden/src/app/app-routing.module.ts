498aa52bc51da468473097c36822f80456917a6f
unknown <marcodiego21.03@gmail.com>
2020-12-15 15:58:35 -0600

    First commit

--- garden/src/app/app-routing.module.ts
+++ garden/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent },
+  {path: '**', component: ErrorpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- garden/src/app/app.module.ts
+++ garden/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ErrorpageComponent
   ],
   imports: [
     BrowserModule,
